ac3fa88067f09791bbcb13e68dad63203a2798d9
Tomas126 <30597891+Tomas126@users.noreply.github.com>
2019-03-10 15:33:06 +0800

    Create JavaScript unshift()函数移入数据到数组第一位.js

---
+++ JavaScript unshift()函数移入数据到数组第一位.js
@@ -0,0 +1,15 @@
+/*任务
+使用 unshift() 函数把["Paul",35] 加入到 myArray 的头部。*/
+// 举例
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); 
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul",35]);
+// Only change code below this line.
+
+
